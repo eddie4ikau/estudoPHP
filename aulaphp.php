0863943e08a6feaf8497865844f3a10ee716d1b2
eddie4ikau <77578708+eddie4ikau@users.noreply.github.com>
2021-01-18 11:48:26 -0300

    Update aulaphp.php

--- aulaphp.php
+++ aulaphp.php
@@ -1,4 +1,16 @@
 <?php
+// echo significa mostrar na tela
+// <br> significa quebra de linha
+// está instrução está sendo realizada no sistema Linux, se for usar no windows é necessario a instalação do git para Widows.
+// estas duas barras antes dos comentarios significam , comentarios 
+// sempre que terminar uma instrução utilize ponto virgula ;
+// As strings são escrita entres aspas duplas ou simples, por enquanto elas ainda não fazem diferenças
+
+
+// espero que a cada progresso, eu retorne neste arquivo e ir atualizando, de forma que este inicio permaneça..
+
+
+
 echo " *Curso aula php <br>";
 echo " *aprendendo e configurando git <br>";
 echo " *primeiros passos <br>";
@@ -15,6 +27,25 @@ echo "então devera digitar o seguinte comando no terminal  $ git add aula.php<b
 echo "para adicionar todos seus arquivos sem precisar fazer um de cada vez no terminal utilize o comando $ git add --all <br>";
 echo "e para confirmar suas configuraçoes, no terminal utilize o comando $ git commit -m  'e o nome que queira atribuir'<br>";
 echo "exemplo: $ git commit -m 'estou criando meu primeiro repositorio'<br>";
+echo "prximo passo = $ git remote add origin e o link do seu repositorio criado no site github <br>";
+echo "exemplo $ git remote add origin https://github.com/eddie4ikau/estudoPHP.git <br>";
+echo "proximo passo é empurrar seu arquivo para a conexão remota, que no caso é o github<br>";
+echo  "se o passo anterior esta ok. este proximo passo ira solicitar que digite seu email e passoword da sua conta no github <br>
+após isso, aparecerá uma mensagem confirmando seu processo da seguinte forma:<br>
+Username for 'https://github.com': bruno.....@gmail.com<br>
+Password for 'https://brunocallthirteen@gmail.com@github.com': <br>
+Enumerating objects: 3, done.<br>
+Counting objects: 100% (3/3), done.<br>
+Delta compression using up to 4 threads<br>
+Compressing objects: 100% (2/2), done.<br>
+Writing objects: 100% (3/3), 778 bytes | 778.00 KiB/s, done.<br>
+Total 3 (delta 0), reused 0 (delta 0)<br>
+To https://github.com/eddie4ikau/estudoPHP.git<br>
+ * [new branch]      master -> master <br>";
+
+echo "Agora iremos editar este arquivo na conexão remota, ou seja editar este arquivo aqui no site do github <br> ";
+echo " ^^ "
+
 
 
 
