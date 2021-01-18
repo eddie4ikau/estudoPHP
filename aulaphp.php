359cbdfb16bd416cadf5630df38a90a93740aa28
Bruno Lucas <brunocallthirteen@gmail.com>
2021-01-17 22:32:39 -0300

    first commit

---
+++ aulaphp.php
@@ -0,0 +1,22 @@
+<?php
+echo " *Curso aula php <br>";
+echo " *aprendendo e configurando git <br>";
+echo " *primeiros passos <br>";
+echo " *criar uma pasta chamado exemplo<br>";
+echo " *acesse a pasta exemplo e abra o terminal e digite o comando $ git init <br>";
+echo " *para verificar se o comando foi aceito, na barra que localiza a pasta que criou adicione /.git <br>";
+echo " *exemplo: a pasta com nome exemplo foi criada em $ /opt/lampp/htdocs/exemplo. para verificar o camando ficara da seguinte forma $ /opt/lampp/htdocs/exemplo/.git<br>";
+echo "aparecer algumas pastas e arquivos, mas não se preoculpe<br>";
+
+echo "agora como armazenar diretorio local em uma sala de espera <br> para poder jogar na conexao remota<br>";
+echo "No terminal utilize o  comando $ git add 'e o nome do seu arquivo' <br>";
+echo "Suponhamos que tenha criadoum arquivo chamado aula.php dentro da pasta exemplo<br>";
+echo "então devera digitar o seguinte comando no terminal  $ git add aula.php<br>";
+echo "para adicionar todos seus arquivos sem precisar fazer um de cada vez no terminal utilize o comando $ git add --all <br>";
+echo "e para confirmar suas configuraçoes, no terminal utilize o comando $ git commit -m  'e o nome que queira atribuir'<br>";
+echo "exemplo: $ git commit -m 'estou criando meu primeiro repositorio'<br>";
+
+
+
+
+?>
